e6442a30c5e1f869d399844cb848bf90643082ef
Ryan Breed <opensource@breed.org>
2017-02-17 12:49:57 -0600

    updated qemu builds for LK 44/49

---
+++ bin/hash_img.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+HASH_ALGO="md5"
+IMG_PATH=$1
+IMG_NAME="$(basename $IMG_PATH)"
+IMG_FILE_HASH="${IMG_NAME}.${HASH_ALGO}"
+IMG_FILE_HASH_PATH="${IMG_PATH}.${HASH_ALGO}"
+
+echo "algo:   $HASH_ALGO"
+echo "image:  $IMG_NAME"
+echo "input:  $IMG_PATH"
+echo "output: $IMG_FILE_HASH_PATH"
+
+function hash_file {
+  path=$1
+  algo=$2
+  if [ -x /usr/bin/openssl ]; then
+    openssl $algo < $path | awk '{print $2}'
+  fi
+}
+  
+if [ -f $IMG_PATH ]; then
+  IMG_HASH=$( hash_file "$IMG_PATH" $HASH_ALGO )
+  echo "${IMG_HASH} ${IMG_NAME}" | tee "${IMG_FILE_HASH_PATH}"
+else
+    echo "image ${IMG_PATH} does not exist"
+fi
